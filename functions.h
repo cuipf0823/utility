563c2f12a598963f77503918987df7fb1bf25c5d
cuipf1989 <570094028@qq.com>
2017-02-16 16:53:32 +0800

    添加堆栈变量判断函数

--- functions.h
+++ functions.h
@@ -45,4 +45,15 @@ void reverse_by_std(char* str, int n)
 {
 	std::reverse(str, str + n);
 }
+
+/*
+判断对象申请在堆上还是栈上
+原理：局部变量onstack放在程序的栈顶，由于栈的结构是向下扩展（趋向于低地址）；且堆地址低于栈地址（依赖于os）
+	  当然无法详细区分静态数据和堆数据，两者都会返回True
+*/
+bool OnHeap(const char* address)
+{
+	char onstack;
+	return address < &onstack;
+}
 #endif
