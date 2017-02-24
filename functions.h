a41f9ee17d5a77c1d10c585fafb14f589643836c
cuipf1989 <570094028@qq.com>
2017-02-24 15:27:24 +0800

    replacespace函数添加

--- functions.h
+++ functions.h
@@ -7,6 +7,7 @@
 #define FUNCTIONS_H
 #include <iostream>
 #include <algorithm>
+#include <string.h>
 using namespace std;
 
 //翻转字符串函数 如：“12345”翻转成“54321”
@@ -114,13 +115,92 @@ double Power(double base, int exponent)
 	}
 }
 
+/**
+ * 	二维数组中查找，每一行从左到右递增，每一列从上到下递增，根据输入的数字判断是否存在
+ * 	存在返回 True;
+ *  1. 选择右上角数字比较；大于查找数字，剔除所在的列；否则剔除所在行；
+ * 
+ */
+bool MatrixFind(int* matrix, int rows, int columns, int number)
+{
+	bool found = false;
+	if (matrix != nullptr && rows > 0 && columns > 0)
+	{
+		int row = 0; 
+		int col = columns - 1;
+		while (row < col && columns > 0)
+		{
+			if (matrix[row * columns + col] == number)
+			{
+				found = true;
+				break;
+			}
+			else if (matrix[row * columns + col] > number)
+			{
+				--col;
+			}
+			else
+			{
+				++row;
+			}
+		}
+	}
+	return found;
+}
 
-//测试
+
+/**
+ *	替换字符串中所有的空格为‘%20’；
+ *  para len: 为字符串的总容量
+ *	思路：先计算字符串中空格的个数，扩展字符串，依次拷贝字符串即可
+ */
+bool ReplaceSpace(char* src, int len)
+{
+	if (src == nullptr || len <= 0)
+	{
+		return false;
+	}
+	int space_num = 0;
+	for (int idx = 0; src[idx] != '\0'; ++idx)
+	{
+		if (src[idx] == ' ')
+		{
+			++space_num;
+		}
+	}
+	int ex_len = strlen(src) + space_num * 2;
+	if (ex_len > len)
+	{
+		return false;
+	}
+	char* rend = src + strlen(src);
+	char* rend_ex = src + ex_len;
+	while (rend != src && space_num > 0)
+	{
+		if (*rend == ' ')
+		{
+			*rend_ex-- = '0';
+			*rend_ex-- = '2';
+			*rend_ex-- = '%';
+		}
+		else
+		{
+			*rend_ex-- = *rend;
+		}
+		--rend;
+	}
+	return true;
+}
+
 void TestFunc()
 {
 	std::cout << Power(2.0, 4) << std::endl;
 	std::cout << Power(0.0, -2) << std::endl;
 	std::cout << Power(3.0, -2) << std::endl;
 
+	const int kLength = 50;
+	char array[kLength] = { "we are so happy !" };
+	ReplaceSpace(array, kLength);
+	std::cout << array << std::endl;
 }
 #endif
